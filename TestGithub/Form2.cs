d72f8b02648ed006568662c4c22475e1b91aea37
李朗 <alex2630044368@outlook.com>
2018-05-21 18:10:37 +0800

    subtest
    修改主分支，尝试副分支是否能同步

--- TestGithub/Form2.cs
+++ TestGithub/Form2.cs
@@ -21,5 +21,10 @@ namespace TestGithub
         {
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form3().Show();
+        }
     }
 }
